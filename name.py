d9281fd9265febdd1749f2bb7694f81b855c05fa
firehead999 <garais.eduard@gmail.com>
2022-04-05 21:44:13 +0300

    reworked file name.py

--- name.py
+++ name.py
@@ -2,7 +2,8 @@ import json
 
 
 def ingredients():
-    keys = list()
+    strIngredient = list()
+    strMeasure = list()
 
     with open('json_obj.json') as file:
         dict_json = json.load(file)  # dict
@@ -10,20 +11,37 @@ def ingredients():
     for key in dict_json['drinks'][0]:  # for all values in json
         if dict_json['drinks'][0][key] is not None:
             if key.startswith('strIngredient'):
-                keys.append(key)
+                strIngredient.append(key)
+            elif key.startswith('strMeasure'):
+                strMeasure.append(key)
+            
+    sum_measure_and_ingredient = list()
 
-    return keys
+    for i in range(len(strIngredient)):
+        sum_measure_and_ingredient.append(dict_json['drinks'][0][strMeasure[i]] + dict_json['drinks'][0][strIngredient[i]])
+
+    return sum_measure_and_ingredient
+        
 
 
-def proportions():
-    keys = list()
+# def proportions():
+#     keys = list()
 
-    with open('json_obj.json') as file:
-        dict_json = json.load(file)  # dict
+#     with open('json_obj.json') as file:
+#         dict_json = json.load(file)  # dict
 
-    for key in dict_json['drinks'][0]:  # for all values in json
-        if dict_json['drinks'][0][key] is not None:
-            if key.startswith('strMeasure'):
-                keys.append(key)
+#     for key in dict_json['drinks'][0]:  # for all values in json
+#         if dict_json['drinks'][0][key] is not None:
+#             if key.startswith('strMeasure'):
+#                 keys.append(key)
 
-    return keys
+#     return keys
+
+# a = ingredients()
+# b = proportions()
+
+# c = map(sum, zip(b,a))
+# print(type(c))
+
+# c = ingredients()
+# print(c)
